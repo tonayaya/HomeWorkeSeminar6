f6ef19ad160f9ce7a552dbe79d83d7e267522446
tonayaya <tonayaya@icloud.com>
2022-11-17 14:51:36 +0400

    Seminar 6 Home Work ex41, ex 43

--- ex1z41/Program.cs
+++ ex1z41/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 41. Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+
+Console.Clear();
+Console.WriteLine($"Задача 41. Cколько чисел больше 0 ввёл пользователь \n");
+Console.Write($"Введи число М(количество чисел): ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[] massiveNumbers = new int[m];
+void InputNumbers(int m)
+{
+for (int i = 0; i < m; i++)
+{
+    Console.Write($"Введи {i+1} число: ");
+    massiveNumbers[i] = Convert.ToInt32(Console.ReadLine());
+}
+}
+int Comparison(int[] massiveNumbers)
+{
+int count = 0;
+for (int i = 0; i < massiveNumbers.Length; i++)
+{
+    if(massiveNumbers[i] > 0 ) count += 1; 
+}
+return count;
+}
+InputNumbers(m);
+Console.WriteLine($"Введено чисел больше 0: {Comparison(massiveNumbers)} ");
